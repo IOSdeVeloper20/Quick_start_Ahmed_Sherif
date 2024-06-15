e9434a33ab27c20d6dd4809717020907ce4f7ecf
IOSdeVeloper20 <81517945+IOSdeVeloper20@users.noreply.github.com>
2024-06-15 20:58:26 +0300

    added tabs js.

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-$('.slick-slider').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
+  //pick all tab
+  const tabs = document.querySelectorAll('.tab');
+  //pick the image div or place
+  const imageElement = document.querySelector('#tabs_img img');
+
+  //images sources in an object
+  const tabImages = {
+    tab_1: 'img/tabs-1.jpg',
+    tab_2: 'img/tabs-2.jpg',
+    tab_3: 'img/tabs-3.jpg'
+  };
+
+  //loop on all the tabs and display an image specified for each tab.
+  tabs.forEach(tab => {
+    //add an click listener for each tab
+    tab.addEventListener('click', () => {
+      //get the id of each tab from the html 
+      const tabId = tab.id;
+      console.log(tabId);
+      //assign the image depending on which tab is clicked.
+      const newImageSrc = tabImages[tabId];
+      //check if it has value or not
+      if (newImageSrc) {
+        //change the src of the image inside the div of the image.
+        imageElement.setAttribute('src', newImageSrc);
       }
-    ]
+    });
   });
